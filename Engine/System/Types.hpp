0c1399b35240c3cd220648ccb032f7b3e5106bcc
K-Oblin <36888398+K-OBLIN@users.noreply.github.com>
2023-12-09 16:56:32 +0900

    Update

---
+++ Engine/System/Types.hpp
@@ -0,0 +1,14 @@
+#ifndef ENGINE_SYSTEM_TYPES_HPP
+#define ENGINE_SYSTEM_TYPES_HPP
+
+#if defined(_MSC_VER)
+    #pragma once
+#endif
+
+#include <cstddef>
+#include <cstdint>
+
+using sbyte_t = std::int8_t;
+using byte_t = std::uint8_t;
+
+#endif
